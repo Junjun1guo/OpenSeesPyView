7f6974c67c5457310a68c47d4291387ef95a121b
Junjun1guo <guojj01@gmail.com>
2022-08-29 21:12:54 +0800

    change the GUI name

---
+++ test.py
@@ -0,0 +1,15 @@
+#-*-coding: UTF-8-*-
+#  Author: Junjun Guo
+#  E-mail: guojj@tongji.edu.cn/guojj_ce@163.com/guojj01@gmail.com
+#  Environemet: Successfully excucted in python 3.8
+#  Date: 2021.04.27
+#import necessary modules
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+
+if __name__ == '__main__':
+    pass
